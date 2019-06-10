f396f3e36f8726386ecb6b3a65f5a614816077d3
Uwe <uwe.jakob@googlemail.com>
2019-06-11 01:31:41 +0200

    api url in enviroment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:3000'
+  // 'http://localhost:5000/api'
+  // 'https://35.194.50.191/api'
 };
 
 /*
